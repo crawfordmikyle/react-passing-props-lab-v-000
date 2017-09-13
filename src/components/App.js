610467e948bdf7a155af0a8be36493c72d2ff185
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-09-13 12:55:18 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,43 @@
 import React from 'react';
-
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+export default class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      fruit: [],
+      filters: [],
+      currentFilter: null
+    }
+  }
 
-export default App;
+  getFruits = () => {
+    fetch('/api/fruit')
+      .then(response => response.json())
+      .then(fruit => this.setState({ fruit }));
+  }
+
+  getFilters = () => {
+    fetch('/api/fruit_types')
+      .then(response => response.json())
+      .then(filters => this.setState({ filters }));
+  }
+
+  componentWillMount() {
+    this.getFilters();
+    this.getFruits();
+  }
+
+  handleFilterChange = (event) => {
+    this.setState({selectedFilter: event.target.value})
+  }
+
+  render(){
+    return (<FruitBasket
+      fruit={this.state.fruit}
+      filters={this.state.filters}
+      currentFilter={this.state.currentFilter}
+      handleChange={this.handleFilterChange.bind(this)} />
+    );
+  }
+}
